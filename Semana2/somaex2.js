00c9e6021538a1773fcd8e42311a7916894413dc
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-11 16:23:48 -0300

    M1S02 ex3

---
+++ Semana2/somaex2.js
@@ -0,0 +1,10 @@
+
+var num1 = window.prompt("Digite um número qualquer");
+var num2 = window.prompt("Digite um segundo número qualquer");
+
+var num3 = parseFloat(num1) + parseFloat (num2) ;
+
+document.write("<h1>" + num3 + "</h1>");
+
+
+
