b5df181e0ea31ac9f9843cc0ef218ec0ed55f7d9
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-11 15:48:26 -0300

    M1S02 Ex 2

---
+++ Semana2/ExSemana2.js
@@ -0,0 +1,6 @@
+
+var nome = window.prompt("Digite seu nome");
+var email = window.prompt("Digite seu email");
+console.log(nome);
+console.log(email);
+document.write("<h4>" + nome + "<br />" + email + "</h4>");
