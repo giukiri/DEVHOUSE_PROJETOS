0d95dd10056db30d413d7c07e7b1af292c80b3e8
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-14 20:41:07 -0300

    M1S03 ex1

---
+++ Semana3/ParImpar.js
@@ -0,0 +1,26 @@
+
+    var num1 = window.prompt("Digite um número qualquer");
+
+var determinarParImpar = function (valor) {
+   return valor % 2 ==1 ;
+}
+
+var x;
+   if( determinarParImpar(num1)){
+       x = 'ímpar'
+       
+    } else{
+      
+      x = 'par'
+        
+    }
+
+   
+console.log(x);
+
+
+
+
+
+
+
