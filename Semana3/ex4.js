d3211e2fa2234a02e7c5d05da01a86d42518b0a9
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-17 19:06:43 -0300

    M1S03 ex 4

---
+++ Semana3/ex4.js
@@ -0,0 +1,28 @@
+
+
+var primeiro = document.getElementById('um')
+primeiro.innerHTML = " O texto 1 vai falar sobre porquê manipular o DOM";
+primeiro.style.color = 'blue';
+
+var segundo = document.getElementById('dois')
+segundo.innerHTML = " O DOM permite a manipulação de objetos na página web a partir da utilização de JS.";
+segundo.style.color = 'darkblue';
+
+var terceiro = document.getElementById('três')
+terceiro.innerHTML = " A sigla DOM siginifica Docment Object Model";
+terceiro.style.color = 'aquablue';
+
+var quarto = document.getElementById('quatro')
+quarto.innerHTML = " A sigla DOM siginifica em português o modelo de documento por objetos.";
+quarto.style.color = 'green';
+
+var quinto = document.getElementById('cinco')
+quinto.innerHTML = "  DOM é uma representação orientada por objetos. ";
+quinto.style.color = 'gray';
+
+
+
+
+
+
+
