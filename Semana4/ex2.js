6c0cc00aa53d1bcebc818d999086fac6ca6df7d6
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-24 20:09:46 -0300

    M1S04 ex2

---
+++ Semana4/ex2.js
@@ -0,0 +1,26 @@
+
+let Filme = {
+    constructor(nome,anoDeLançamento,diretor){
+    nome = "Homem-Aranha: Sem Volta para a Casa";
+    anoDeLançamento = '2021';
+    diretor = 'Jon Watts';
+    }
+
+}
+
+console.log("O filme que escolhi é o " + Filme.nome + "que foi lançado em " + Filme.anoDeLançamento + "e dirigido por " + Filme.diretor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
