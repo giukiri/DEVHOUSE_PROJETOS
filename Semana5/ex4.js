5536c13c42dedb3e0bd81507181b0fff7b1775a1
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-29 20:42:39 -0300

    M1S05 ex4

---
+++ Semana5/ex4.js
@@ -0,0 +1,24 @@
+
+
+const numeros = [-1,3,-4,2,5,6,-90,50,-76];
+
+const numerosNegativos = numeros.filter((numeros) => numeros < 0 )
+
+console.log(numerosNegativos);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
