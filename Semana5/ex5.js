7a6f8069942d1ea70a4b41a9edbc410ea42177d4
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-07-29 22:38:57 -0300

    M1S05 ex5

---
+++ Semana5/ex5.js
@@ -0,0 +1,16 @@
+//Utilize o if ternário para construir uma função que recebe o número do dias e retorna se pertence a primeira ou a segunda quinzena
+
+
+var dia = 4;
+var resultado = dia < 16 ? "Primeira Quinzena" : "Segunda Quinzena";
+
+ console.log(resultado);
+
+
+
+
+
+
+
+
+
