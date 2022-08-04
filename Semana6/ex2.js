fef2995b4a879c33acff622a4aabdcefbd285daa
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-08-04 01:27:44 -0300

    M1S06 ex2

--- Semana6/ex2.js
+++ Semana6/ex2.js
@@ -8,8 +8,16 @@
 let array1 = [12,43,6,2,8,5,3];
 let array2 = ["1", "2", "3", "4"];
 
-const array3 = [...array1,...array2];
+const array3 = [...array1,...array2]
 console.log(array3)
+
+const array4 = [array1,array2];
+
+console.log(...array4);
+
+
+
+
 
 
 
