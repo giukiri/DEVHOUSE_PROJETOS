ae26fcb657d2bc6c6a8b2d4e7d96aaea0b6efbf6
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-08-24 22:51:40 -0300

    M1S08 ex5

---
+++ Semana8/primeiro-projeto/src/componentes/soma.js
@@ -0,0 +1,15 @@
+
+const Soma = ({num1,num2}) => {
+
+    let somaF ;
+    somaF = Number(num1) + Number(num2) 
+ 
+   return (
+    <p> A soma dos números é : {(somaF)} </p>
+   )
+    
+        
+        
+}
+
+export default Soma;
