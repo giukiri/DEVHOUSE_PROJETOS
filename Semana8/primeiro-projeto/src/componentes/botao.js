3d525bd2f8b4ab463add8a85163bcbc2c06649a5
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-08-29 15:49:05 -0300

    M1S08 ex6

---
+++ Semana8/primeiro-projeto/src/componentes/botao.js
@@ -0,0 +1,18 @@
+
+
+const Btn = () => {
+
+    return (
+    <button style={styles} >Click</button>
+    );
+}
+
+export default Btn
+
+const styles = {
+  
+        backgroundColor: 'red'
+
+    
+}
+
